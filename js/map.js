a9a5098806cebe558814b3742af74868b3804813
unknown <jaircandal@gmail.com>
2023-05-16 21:59:59 -0300

    Arreglo estilo pagina Contacto

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ function initMap() {
     const coord = { lat: -34.6037389, lng: -58.3815704 }
     const map = new google.maps.Map(mapDiv, {
         center: coord,
-        zoom: 14,
+        zoom: 15,
     });
     
     const marker = new google.maps.Marker({
