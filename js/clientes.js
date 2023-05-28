8d2f48fa6bd2f0b7372af0d1eeb65edc9ab928d9
Virginia Vasquez <virginiav@gmail.com>
2023-05-28 19:58:57 -0300

    arreglo visualización de salida desde api externa en Clientes. Redondeo de borde del iframe en Nosotros.

--- js/clientes.js
+++ js/clientes.js
@@ -13,7 +13,7 @@ function llamarApi(){
 }
 
 function mostrarHTML(data){
-  let top =5;
+  let top =9;
   let table;
   if (data.lengt < top) {
     top = data.lengt
@@ -23,21 +23,13 @@ function mostrarHTML(data){
     let comentario = `<td>${data[i].body}</td>`;
     respuesta.innerHTML += `<tr>${cliente + comentario}</tr>`;
   };
-  // data.forEach(item => {
-  //   const row = document.createElement('tr');
-  //   row.innerHTML = `
-  //     <td>${item.title}</td>
-  //     <td>${item.body}</td>
-  // `
-  // respuesta.appendChild(row)
-  // });
-  
+   
   mostrarHTML2(data,top)
    
 }
 
 function mostrarHTML2(data,n){
-  let top =11;
+  let top =14;
   let table;
   if (n === 5 && data.lengt < top) {
     top = data.lengt
